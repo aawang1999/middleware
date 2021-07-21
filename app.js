56c5f5d700119ff28f5992a08282a3d47140ca8b
Tristan Wang <aawang99@gmail.com>
2021-07-21 10:34:27 -0700

    feat: add log request and response

--- app.js
+++ app.js
@@ -1,36 +1,42 @@
 const express = require('express')
+const exphbs = require('express-handlebars')
+const { randomId, logRequest, logResponse } = require('./tools')
 const app = express()
 const port = 3000
 
-function toLocalTime(date) {
-  return new Date(date.getTime() - (date.getTimezoneOffset() * 60 * 1000))
-    .toISOString()
-    .replace('T', ' ')
-    .replace(/\.\d{3}Z$/, '')
-}
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 
-app.use((req, res, next) => {
-  const start = new Date()
-  console.log(`${toLocalTime(start)} | ${req.method} from ${req.url}`)
+app.set('view engine', 'hbs')
+
+app.use(function logMiddlewareInfo(req, res, next) {
+  const [requestLog, requestUnixTime] = logRequest(req)
+  console.log(requestLog)
+  res.on('finish', () => {
+    const responseLog = logResponse(res, requestUnixTime)
+    console.log(responseLog)
+  })
   next()
 })
 
 app.get('/', (req, res) => {
-  res.send('列出全部 Todo')
+  const id = randomId()
+  res.render('index', { id })
 })
 
 app.get('/new', (req, res) => {
-  res.send('新增 Todo 頁面')
+  const id = randomId()
+  res.render('index', { id })
 })
 
 app.get('/:id', (req, res) => {
-  res.send('顯示一筆 Todo')
+  const id = randomId()
+  res.render('index', { id })
 })
 
 app.post('/', (req, res) => {
-  res.send('新增一筆 Todo')
+  res.redirect('/')
 })
 
 app.listen(port, () => {
-  console.log(`App running on port ${port}`)
+  console.log(`Express is running on port ${port}.`)
 })
---
+++ tools.js
@@ -0,0 +1,19 @@
+const moment = require('moment')
+
+const tools = {
+  randomId: () => {
+    return Math.floor(Math.random() * 100)
+  },
+  logRequest: (req) => {
+    const requestUnixTime = Date.now()
+    const requestTimeLog = moment(requestUnixTime).format('YYYY-MM-DD HH:mm:ss')
+    const requestLog = `${requestTimeLog} | ${req.method} from ${req.originalUrl}`
+    return [requestLog, requestUnixTime]
+  },
+  logResponse: (res, requestUnixTime) => {
+    const responseUnixTime = Date.now()
+    const responseTimeLog = moment(responseUnixTime).format('YYYY-MM-DD HH:mm:ss')
+    const handleDuration = responseUnixTime - requestUnixTime
+    return `${responseTimeLog} | ${res.req.method} from ${res.req.originalUrl} | total time: ${handleDuration}ms`
+  }
+}
